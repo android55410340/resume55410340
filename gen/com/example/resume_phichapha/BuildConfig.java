b9758458cf244b9301cae137ef3be391f408b2be
android55410340 <phichapha-1@hotmail.com>
2014-09-02 17:20:26 +0700

    Resume

---
+++ gen/com/example/resume_phichapha/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.resume_phichapha;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/resume_phichapha/ImageActivity.java
@@ -0,0 +1,15 @@
+package com.example.resume_phichapha;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ImageActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.image_phichapha);
+	}
+	}
+
+
